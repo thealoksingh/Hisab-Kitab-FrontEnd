f3179ec61a71d984477763c59cff09bc26915d47
cyboravi <cyboravidell@gmail.com>
2025-08-26 14:37:34 +0530

    changes the web socket url with env variable

--- src/hooks/useNotificationSubscription.js
+++ src/hooks/useNotificationSubscription.js
@@ -8,7 +8,7 @@ export const useNotificationSubscription = (userId, onNotification) => {
   useEffect(() => {
     if (!userId) return;
 
-    const socket = new SockJS("http://localhost:8080/Hisab-Kitab/ws");
+    const socket = new SockJS(`${process.env.REACT_APP_API_BASE_URL}/ws`);
     const client = new Client({
       webSocketFactory: () => socket,
       reconnectDelay: 5000,
